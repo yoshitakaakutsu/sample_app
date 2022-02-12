2125564c85ea177131b1129152dca4e432220e7d
EC2 Default User <ec2-user@ip-172-31-38-48.ec2.internal>
2022-02-12 11:09:18 +0000

    create_hoat/model/controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "aea49921338f40729d9c7d5368902c23.vfs.cloud9.us-east-1.amazonaws.com"
 end
